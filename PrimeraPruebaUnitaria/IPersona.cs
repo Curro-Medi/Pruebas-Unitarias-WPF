9e5c3acf17117ec7e998607a0059f54cb75e9692
Curro Mediavilla <59694404+Curro-Medi@users.noreply.github.com>
2020-12-27 01:19:53 +0100

    Add files via upload

---
+++ PrimeraPruebaUnitaria/IPersona.cs
@@ -0,0 +1,22 @@
+﻿namespace PrimeraPruebaUnitaria
+{
+    public interface IPersona
+    {
+        void conseguirPareja(IPersona p);
+        void copiaPersona(IPersona p1);
+        void dejarPareja(IPersona p);
+        string getApellido();
+        int getEdad();
+        int getKmRecorridos();
+        string getNombre();
+        Persona getPareja();
+        void morir();
+        void nacer();
+        bool getVivo();
+        string nombreCompleto();
+        int salirACorrer(int km);
+        void setApellido(string a);
+        void setEdad(int e);
+        void setNombre(string n);
+    }
+}
